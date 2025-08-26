37ee02486854a0f31dc304ba34faf71569e92e61
“Dimitri” <dimitri.s.amado@gmail.com>
2025-08-26 11:06:05 -0300

    :sparkles: feat: Add database model for User with SQLAlchemy integration

---
+++ src/models/user.py
@@ -0,0 +1,8 @@
+from database import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
