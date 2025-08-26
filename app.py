0a25284d69e8e2f0b7b6af2f919f979b7c33c29f
“Dimitri” <dimitri.s.amado@gmail.com>
2025-08-26 11:34:35 -0300

    :sparkles: feat: Implement login route with user authentication

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 from flask import Flask
+from src.models.user import User
+from flask_login import LoginManager, login_user, current_user
 from database import db
+from flask import request, jsonify
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key_here"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Autenticação realizada com sucesso!"})
+
+    return jsonify({"message": "Credenciais Inválidas"}), 401
 
 
 @app.route("/", methods=["GET"])
