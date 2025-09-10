5ef3273853f935c3df966e392afc8e6cc55e9854
Dimitri <dimitri.s.amado@gmail.com>
2025-09-10 20:59:01 -0300

    :sparkles: feat: Update database configuration to use MySQL and add Docker support

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import request, jsonify
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key_here"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:admin123@127.0.0.1:3306/flask-crud"
 
 login_manager = LoginManager()
 db.init_app(app)
